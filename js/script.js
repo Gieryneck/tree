adb92ffc2bffa2e5b47b2b45a9decf96d17b6c96
Gieryn <gierynt@gmail.com>
2017-10-08 21:14:20 +0200

    Created the repo

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+function drawTree(h) {
+
+	for(i = 0; i < h; i++) {
+
+		var star = '';
+
+		for(j = 0; j <= i; j++) {
+
+			star += '*';
+
+		}
+
+		console.log(star);	
+	}
+} 
+
+drawTree(10);
